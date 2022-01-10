ba3a712c4d1b3fc95e7825c20eaf218e99be8f0a
jmeisele <jmeisele@yahoo.com>
2022-01-09 19:10:47 -0600

    celery config env vars must be present in API

--- backend/workers/config.py
+++ backend/workers/config.py
@@ -5,9 +5,5 @@ from pydantic import BaseSettings
 class CelerySettings(BaseSettings):
     BROKER_URL: str = os.environ["BROKER_URL"]
     BACKEND_URL: str = os.environ["BACKEND_URL"]
-    # BROKER_URL: str = os.getenv("BROKER_URL")
-    # BACKEND_URL: str = os.getenv("BACKEND_URL")
-    # BROKER_URL: str = 'redis://redis:6379/0'
-    # BACKEND_URL: str = 'redis://redis:6379/0'
 
 settings = CelerySettings()
